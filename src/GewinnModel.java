3901dc31f56ebe4ed997076c7bbf23f8a8b00906
niklas <nandrae@student.tgm.ac.at>
2026-09-23 13:11:19 +0200

    Grundgerüst und Konstruktor

---
+++ src/GewinnModel.java
@@ -0,0 +1,12 @@
+import java.util.Random;
+
+public class GewinnModel {
+    private int gesamtPunkte;
+    private int spielerZahl;
+    private int computerZahl;
+    private int rundenErgebnis;
+
+    public GewinnModel() {
+        this.gesamtPunkte = 30; // Startpunkte laut Regeln
+    }
+}
